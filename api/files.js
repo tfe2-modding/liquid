42c85f22097ba935f5ca2e1ffe3b4b8a276267c6
DT <66632141+DT-is-not-available@users.noreply.github.com>
2024-10-28 10:43:28 -0400

    oopsie

--- api/files.js
+++ api/files.js
@@ -4,13 +4,15 @@ Liquid.getModResource = function(filepath) {
 	return Liquid._modFiles[Liquid.getModID(1)].files[fixedfilepath]
 }
 
-Liquid.getInfoFile = function(filename) {
-	let topinfo = []
-	for (const files of Object.values(Liquid._modFiles)) {
-		let info = files.filesByName[filename]
-		if (info) for (let i = 0; i < info.length; i++) {
-			topinfo = topinfo.concat(info[i])
+Liquid.onInfoFiles = function(filename, callback) {
+	ModTools.onModsLoaded(function(game) {
+		let topinfo = []
+		for (const files of Object.values(Liquid._modFiles)) {
+			let info = files.filesByName[filename]
+			if (info) for (let i = 0; i < info.length; i++) {
+				topinfo = topinfo.concat(info[i])
+			}
 		}
-	}
-	return topinfo
+		callback(topinfo)
+	})
 }
